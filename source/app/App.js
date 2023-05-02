556bc46deabb8e944e85b13037e93df74fefc31d
Josué R.R <73318183+JosueRR@users.noreply.github.com>
2023-05-01 20:23:44 -0600

    Modifica App & Index para desplegar la pagina de parcelas

--- source/app/App.js
+++ source/app/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import {Page} from './reservation_page/Reservation_page';
+import Parcelas from './reservation_page/Parcelas';
 
-const Home = () => {
+//import { Routes, Route } from 'react-router-dom';
+//import {Page} from './reservation_page/Reservation_page';
+
+/* const Home = () => {
    const handleClick = () => {
       window.location.href = '/reservation';
    };
@@ -12,15 +14,12 @@ const Home = () => {
          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleClick}>Book Now</button>
       </div>
    );
-}
+} */
 
 const App = () => {
   return (
    <div>
-      <Routes>
-         <Route exact path="/" element={<Home />} />
-         <Route path="/reservation" element={<Page />} />
-      </Routes>
+      <Parcelas />
    </div>
   );
 }
