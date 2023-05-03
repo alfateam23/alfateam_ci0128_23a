a88c615a6cad8f378956de2557b0c2dc0662ae5d
Ariel <ariel@debian-BULLSEYE-live-builder-AMD64>
2023-05-02 19:02:02 -0600

    Adds comments so Jason and jose know where to add their parts

--- source/app/reservation_page/Reservation_page.js
+++ source/app/reservation_page/Reservation_page.js
@@ -13,6 +13,10 @@ const Select_dates_title = () => {
   );
 };
 
+/*
+Function to show the page where the traveller will select the lot
+*/
+
 export const Parcel_page = () => {
   return (
     <div>
@@ -20,11 +24,18 @@ export const Parcel_page = () => {
       <br />
       <Reservation_type />
       <br />
-      <NavBar_PIR selected={"parcel"}/>
+      <NavBar_PIR selected={"lot"}/>
+      <br />
+      {/*Jose's part goes here*/}
+      <Next_link route_next='/reservation/info'
+      route_back='/reservation/availability'/>
     </div>
   );
 };
 
+/*
+Function to add the form for the traveller to fill
+*/
 export const T_information = () => {
   return (
     <div>
@@ -33,9 +44,17 @@ export const T_information = () => {
       <Reservation_type />
       <br />
       <NavBar_PIR selected={"traveller"}/>
+      <br />
+      {/*Jason's part goes here*/}
+      <Next_link route_next='/reservation/review'
+      route_back='/reservation/lot'/>
     </div>
   );
 };
+
+/*
+Function to show the review page
+*/
 
 export const Review = () => {
   return (
@@ -45,10 +64,16 @@ export const Review = () => {
       <Reservation_type />
       <br />
       <NavBar_PIR selected={"review"}/>
+      <br />
+      <Next_link route_next='/'
+      route_back='/reservation/info' />
     </div>
   );
 };
 
+/*
+Page where the user can see the availability of the dates selected for the visit
+*/
 export const Availability_page = () => {
   let quantity = 100;
   return (
@@ -62,22 +87,33 @@ export const Availability_page = () => {
       <Spaces_left quantity={quantity}/>
       <br />
       <Party_title />
-      <Next_link route='/reservation/parcel'/>
+      <Next_link route_next='/reservation/lot'
+      route_back='/reservation/'/>
     </div>
   );
 };
 
+/**
+ * Page where the user can set the dates of the visit
+ */
+
 export const Select_dates_page = () => {
+  const [active, setActive] = useState(false);
+  function handleClick() {
+    setActive(!active);
+  }
+
   return (
     <div>
       <Select_dates_title />
       <br/>
       <Reservation_type />
-      <From_until />
+      <From_until handleClick={handleClick} />
       <br/>
-      <My_Calendar />
+      <My_Calendar active={active}/>
       <br/>
-      <Next_link route='/reservation/availability'/>
+      <Next_link route_next='/reservation/availability'
+      route_back='/'/>
     </div>
   );
 };
