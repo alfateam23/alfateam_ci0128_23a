4f8084cf49a4c76af3375452a139b3b085f3dbe3
Marco Piedra Venegas <mpiedrav@proton.me>
2023-05-21 05:21:19 -0600

    Login backend

--- design/database/createsequences.py
+++
@@ -1,9 +0,0 @@
-for i in ['N', 'E']:
-    for j in ['C', 'P']:
-        for k in range(65, 74):
-            sequence_name = f"Consecutivo{i}{j}{chr(k)}"
-            print(f"CREATE SEQUENCE {sequence_name}\
-                  \n  START WITH 0\
-                  \n  INCREMENT BY 1\
-                  \n  MAXVALUE 99999999\
-                  \nGO\n")
--- design/database/dropsequences.py
+++
@@ -1,5 +0,0 @@
-for i in ['N', 'E']:
-    for j in ['C', 'P']:
-        for k in range(65, 74):
-            sequence_name = f"Consecutivo{i}{j}{chr(k)}"
-            print(f"DROP SEQUENCE {sequence_name}\nGO")
