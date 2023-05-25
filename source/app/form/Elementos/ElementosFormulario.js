89c165240143954c0d104e0b6e01cecba4468b08
HardJas88 <jason.murillo@ucr.ac.cr>
2023-05-24 21:57:30 -0600

    Se agrega estilos de los botones de checkbox y dropDown

--- source/app/form/Elementos/ElementosFormulario.js
+++ source/app/form/Elementos/ElementosFormulario.js
@@ -11,14 +11,15 @@ npm install babel-plugin-styled-components --save-dev
 
 import styled, { css } from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import { Dropdown } from "antd";
+import { Checkbox } from "antd";
 // aqui van los estilos de los elementos del formulario
 
 //Se utiliza para tener colores predefinidos para toda la app
 const colores = {
   borde: "#0075ff",
   error: "#bb2929",
-  exito: "#1ed12d"
+  exito: "#1ed12d",
 };
 
 //estilo para etiqueta formulario
@@ -167,6 +168,9 @@ const Boton = styled.button`
   &:hover {
     box-shadow: 0px 3.2px 3.2px rgba(0, 0, 0, 0.25);
   }
+  &:disabled {
+    pointer-events: none; /* Desactiva los eventos del puntero */
+  }
 `;
 
 const MensajeExito = styled.p`
@@ -220,6 +224,84 @@ const ButtonIncDec = styled.button`
   }
 `;
 
+const ButtonDropDown = styled.button`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  padding: 5px 10px;
+  background-color: ${colores.borde};
+  color: #ffffff;
+  font-size: 18px;
+  font-weight: 500;
+  text-transform: uppercase;
+  border-radius: 3px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  height: 45px;
+  width: 100%;
+  white-space: nowrap; /* Evita el salto de línea del texto */
+  overflow: hidden; /* Oculta el texto que excede el ancho del botón */
+  text-overflow: ellipsis; /* Muestra puntos suspensivos (...) cuando el texto excede el ancho del botón */
+
+  &:hover {
+    background-color: #0056b3;
+  }
+
+  &:active {
+    background-color: #007bff;
+  }
+
+  &:focus {
+    outline: none;
+    box-shadow: 0 0 0 3px rgba(33, 150, 243, 0.3);
+  }
+  @media (max-width: 800px) {
+    grid-column: span 1;
+  }
+`;
+
+const DropDown = styled(Dropdown)`
+  .ant-dropdown-trigger {
+    height: 45px;
+  }
+
+  .ant-dropdown-menu {
+    background-color: #fff;
+    color: #000;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.15);
+    border-radius: 3px;
+  }
+
+  .ant-dropdown-menu-item {
+    padding: 8px 20px;
+    font-size: 16px;
+  }
+
+  .ant-dropdown-menu-item:hover {
+    background-color: ${colores.borde};
+    color: #fff;
+  }
+`;
+
+const CheckBox = styled(Checkbox.Group)`
+  .ant-checkbox-wrapper {
+    display: inline-flex;
+    align-items: center;
+    cursor: pointer;
+    font-size: 16px;
+  }
+  .ant-checkbox {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    width: 20px;
+    height: 20px;
+    border-radius: 3px;
+    border: 2px solid #0075ff;
+    transition: 0.3s ease all;
+  }
+`;
+
 export {
   Formulario,
   Label,
@@ -232,5 +314,8 @@ export {
   Boton,
   MensajeExito,
   MensajeError,
-  ButtonIncDec
+  ButtonIncDec,
+  ButtonDropDown,
+  DropDown,
+  CheckBox,
 };
