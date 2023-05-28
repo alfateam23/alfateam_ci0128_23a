495c462429b0b3f7e27cd5fed124e19998e12afb
Marco Piedra Venegas <marco.piedra@ucr.ac.cr>
2023-05-28 14:32:33 -0600

    Revert "Feat/backend"

--- source/app/App.js
+++ source/app/App.js
@@ -1,31 +1,22 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
-import {
-   Availability_page, Parcel_page,
-   Review,
-   Select_dates_page,
-   T_information
-} from './reservation_page/Reservation_page';
+import {Availability_page, Parcel_page,
+      Review,
+      Select_dates_page,
+      T_information} from './reservation_page/Reservation_page';
 import { UserData } from './UserData';
 import { Visitors } from './visitors/Visitors';
 import { Page } from './reservation_page/Reservation_page';
-import Dashboard from './admin_dashboard/Dashboard';
 
 /**
- * Temp navbar of project
+ * Home component, for now it just shows a button to send the user to the reserve
+ * module
  */
 const Home = () => {
    return (
       <div>
-         <h1 className="flex justify-center items-center  text-4xl p-4"> Hola! Esta es la página de inicio</h1>
-         <ul className="hidden md:flex">
-            <li className="p-4">
-               <a className="transition ease-in-out hover:text-[#ffaf00] hover:duration-700 cursor-pointer" href="/Dashboard">Dashboard</a>
-            </li>
-            <li className="p-4">
-               <a className="transition ease-in-out hover:text-[#ffaf00] hover:duration-700 cursor-pointer" href="/reservation">Reserva Ahora</a>
-            </li>
-         </ul>
+         <Link to="/reservation" className="bg-blue-500 hover:bg-blue-700
+         text-white font-bold py-2 px-4 rounded">Reserva Ahora</Link>
       </div>
    );
 }
@@ -36,25 +27,25 @@ const Home = () => {
  */
 const App = () => {
    const userData = new UserData();
-   return (
-      <div>
-         <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route path="/reservation" element={<Select_dates_page
-               UserData={userData} />} />
-            <Route path="/reservation/availability" element={<Availability_page
-               UserData={userData} />} />
-            <Route path="/reservation/lot" element={<Parcel_page
-               UserData={userData} />} />
-            <Route path="/reservation/info" element={<T_information
-               UserData={userData} />} />
-            <Route path="/reservation/review" element={<Review
-               UserData={userData} />} />
-            <Route path="/visitors" element={<Visitors />} />
-            <Route path='/Dashboard' element={<Dashboard />} />
-         </Routes>
-      </div>
-   );
+  return (
+   <div>
+      <Routes>
+         <Route exact path="/" element={<Home />} />
+         <Route path="/reservation" element={<Select_dates_page
+            UserData={userData}/>} />
+         <Route path="/reservation/availability" element={<Availability_page
+         UserData={userData} />} />
+         <Route path="/reservation/lot" element={<Parcel_page
+         UserData={userData} />} />
+         <Route path="/reservation/info" element={<T_information
+         UserData={userData} />}/>
+         <Route path="/reservation/review" element={<Review
+         UserData={userData} />}/>
+         <Route path="/visitors" element={<Visitors />} />
+
+      </Routes>
+   </div>
+  );
 }
 
 export default App;
--- source/dbschema/Tests.sql
+++
@@ -1,7 +0,0 @@
-select * from Usuario
-select * from Cliente
-select * from Reservacion
-select * from Telefono
-delete from Reservacion where Codigo = 2
-delete from Cliente where Email = 'este-bandido@gmail.com'
-delete from Usuario where PrimerNombre = 'Esteban'
