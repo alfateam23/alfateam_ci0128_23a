bcfddd17a6e22fca25fe9d24d409168d7ab8847d
Marco Piedra Venegas <mpiedrav@proton.me>
2023-05-30 14:11:43 -0600

    SQL queries for reports

--- source/backend/reports/reportsSelect.js
+++ source/backend/reports/reportsSelect.js
@@ -4,10 +4,18 @@ const db = require('../DbConfig')
 
 export async function selectVisitsInDateRange(startdate, enddate) {
     try {
-        const query = `SELECT TipoProcedencia, TipoVisita, Estatus, CategoriaPago, CantidadVisitantes FROM Visitante
-            JOIN Reservacion
-            ON Codigo=Visitante.CodigoReservacion
-            WHERE Reservacion.FechaInicio BETWEEN ${startdate} AND ${enddate}`
+        const query = `SELECT
+        Reservacion.FechaInicio,
+        Visitante.TipoProcedencia,
+        Visitante.TipoVisita,
+        Visitante.Estatus,
+        Visitante.CategoriaPago,
+        Visitante.CantidadVisitantes
+        FROM Visitante
+        JOIN Reservacion
+        ON Codigo=Visitante.CodigoReservacion
+        WHERE Reservacion.FechaInicio
+        BETWEEN ${startdate} AND ${enddate}`
         const result = await db.executeQuery(query)
         return result
     }
@@ -18,16 +26,23 @@ export async function selectVisitsInDateRange(startdate, enddate) {
 
 export async function selectProfitsInDateRange(startdate, enddate) {
     try {
-        const query = `SELECT TipoProcedencia, TipoVisita, Estatus, CategoriaPago,
-        CantidadVisitantes * TipoVisitanteMonto, TipoVisitante.Moneda FROM Visitante
-            JOIN Reservacion
-            ON Codigo=Visitante.CodigoReservacion
-            JOIN TipoVisitante
-            ON TipoVisitante.TipoProcedencia=Visitante.TipoProcedencia
-            AND TipoVisitante.TipoVisita=Visitante.TipoVisita
-            AND TipoVisitante.Estatus=Visitante.Estatus
-            AND TipoVisitante.CategoriaPago=Visitante.CategoriaPago
-            WHERE Reservacion.FechaInicio BETWEEN ${startdate} AND ${enddate}`
+        const query = `SELECT
+        Reservacion.FechaInicio,
+        TipoVisitante.TipoProcedencia,
+        TipoVisitante.TipoVisita,
+        TipoVisitante.Estatus,
+        TipoVisitante.CategoriaPago,
+        TipoVisitante.Monto * Visitante.CantidadVisitantes AS Ingresos,
+        TipoVisitante.Moneda FROM Visitante
+        JOIN Reservacion
+        ON Codigo=Visitante.CodigoReservacion
+        JOIN TipoVisitante
+        ON TipoVisitante.TipoProcedencia=Visitante.TipoProcedencia
+        AND TipoVisitante.TipoVisita=Visitante.TipoVisita
+        AND TipoVisitante.Estatus=Visitante.Estatus
+        AND TipoVisitante.CategoriaPago=Visitante.CategoriaPago
+        WHERE Reservacion.FechaInicio
+        BETWEEN ${startdate} AND ${enddate}`
         const result = await db.executeQuery(query)
         return result
     }
