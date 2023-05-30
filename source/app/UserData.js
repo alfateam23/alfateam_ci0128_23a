c693044c798e6759e610663bf2f1f6336eff3b49
Jason Josue Murillo Madrigal <b54956@ecci.ucr.ac.cr>
2023-05-30 16:02:56 -0600

    Se actualiza branch co ultimas modificaciones del develop y se conecta con la base de datos

--- source/app/UserData.js
+++ source/app/UserData.js
@@ -4,23 +4,23 @@
 
 export class UserData {
   constructor() {
-    this.start_date = '';
-    this.end_date = '';
-    this.reservation_type = 0; // 1=parcel 2=picnic
+    this.start_date = new Date();
+    this.end_date = new Date();
     this.parcelaSeleccionada = new String();
     this.num_guests = 0;
 
     
-    this.nameUser = '';
-    this.secondName = '';
-    this.firstSurname = '';
-    this.secondSurname = '';
-    this.id = '';
-    this.mail = '';
-    this.phone = [];
-    this.plates = [];
-    this.originCountry = '';
-    this.originProvince = '';
+    this.nameUser = new String();
+    this.firstSurname = new String();
+    this.secondSurname = new String();
+    this.mail = new String();
+    this.totalPlates = 0;
+    this.plate1 = new String();
+    this.plate2 = new String();
+    this.plate3 = new String();
+    this.plate4 = new String();
+    this.plate5 = new String();
+    this.plate6 = new String();
 
     this.TotalPeople = 0;
     this.countAdultNac = 0;
--- source/app/reservation_page/Availability_page.js
+++ source/app/reservation_page/Availability_page.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 
 /**
  * Component to show the title for the availability page
