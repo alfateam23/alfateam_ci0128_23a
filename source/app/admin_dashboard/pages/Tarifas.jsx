95f2dbed15ede265fec388e0f6307adde7b85b4c
Josué R.R <73318183+JosueRR@users.noreply.github.com>
2023-06-03 17:59:59 -0600

    Conecta backend de Tarifas con el front, se falta integrar el editar, todo lo demas sirve

--- source/app/admin_dashboard/pages/Tarifas.jsx
+++ source/app/admin_dashboard/pages/Tarifas.jsx
@@ -1,127 +1,103 @@
 import React from "react";
-import { Card, Typography } from "@material-tailwind/react";
 import 'flowbite';
 
-
-const TABLE_HEAD = ["Procedencia", "Tipo", "Edad", "Categoria de Pago", "Monto", "Modena",""];
-
-const TABLE_ROWS = [
-    {
-        Concepto: "Camping Nacional Adulto",
-        Monto: "CRC",
-    },
-    {
-        Concepto: "Camping Nacional Niño",
-        Monto: "CRC",
-    },
-    {
-        Concepto: "Camping Extranjero Adulto",
-        Monto: "CRC",
-    },
-    {
-        Concepto: "Camping Extranjero Niño",
-        Monto: "CRC",
-    },
-    {
-        Concepto: "Picnic Nacional Adulto",
-        Monto: "CRC",
-    },
-    {
-        Concepto: "Picnic Nacional Niño",
-        Monto: "CRC",
-    },
-    {
-        Concepto: "Picnic Extranjero Adulto",
-        Monto: "CRC",
-    },
-    {
-        Concepto: "Picnic Extranjero Niño",
-        Monto: "CRC",
-    },
-];
+const TABLE_HEAD = ["Procedencia", "Tipo", "Edad", "Categoria de Pago", "Monto", "Modena", ""];
 
 const Tarifas = () => {
-    const [data, setData] = React.useState(null);
+    const [data, setData] = React.useState(null); // use state
+    let tableRows = []; // rows of table
 
-    React.useEffect(() => {
-        fetch("/api")
-            .then((res) => res.json())
-            .then((data) => setData(data.message));
-    }, []);
-
-    async function getGet() {
+    /* Fetch for endpoint of tarifas */
+    const handleTarifas = () => {
         fetch("/tarifas")
             .then((res) => res.json())
-            .then((data) => setData(data.info));
+            .then((data) => setData(data));
+    }
+
+    /* Set state */
+    React.useEffect(() => {
+        handleTarifas()
+    }, []);
+
+
+    console.log('Ojito pa ' + tableRows.length);
+
+    // This renders every row for the table, with specific info
+    const renderTarifa = () => {
+        // Pueden sacar aquí la parte de mapping para tener código más legible
     }
 
     return (
         <div>
             <h1 className="font-sans text-4xl rounded-none py-4 m-3"> Tarifas </h1>
-            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 px-6">
-                    {/* Table Head */}
-                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                        <tr>
-                            <th scope="col" class="px-6 py-3">
-                                {TABLE_HEAD[0]}
-                            </th>
-                            <th scope="col" class="px-6 py-3">
-                                {TABLE_HEAD[1]}
-                            </th>
-                            <th scope="col" class="px-6 py-3">
-                                <span class="sr-only">{TABLE_HEAD[2]}</span>
-                            </th>
-                        </tr>
-                    </thead>
-                    {/* Table Rows */}
-                    <tbody>
-                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Apple MacBook Pro 17"
-                            </th>
-                            <td class="px-6 py-4">
-                                $2999
-                            </td>
-                            <td class="px-6 py-4 text-right">
-                                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                            </td>
-                        </tr>
-                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Microsoft Surface Pro
-                            </th>
-                            <td class="px-6 py-4">
-                                $1999
-                            </td>
-                            <td class="px-6 py-4 text-right">
-                                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                            </td>
-                        </tr>
-                        <tr class="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Magic Mouse 2
-                            </th>
-                            <td class="px-6 py-4">
-                                $99
-                            </td>
-                            <td class="px-6 py-4 text-right">
-                                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
+            {/* Waits for data to be charged */}
+            {/* Esta tabla es un componente de flowbite, por eso algunos tags están definidos con esos atributos */}
+            {data ? (
+                <div className="table-container">
+                    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 px-6">
+                            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" className="px-6 py-3">
+                                        {TABLE_HEAD[0]}
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        {TABLE_HEAD[1]}
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        {TABLE_HEAD[2]}
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        {TABLE_HEAD[3]}
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        {TABLE_HEAD[4]}
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        {TABLE_HEAD[5]}
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        <span className="sr-only">{TABLE_HEAD[6]}</span>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {data.map((tarifa, index) => (
+                                    <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" >
+                                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            {tarifa.TipoProcedencia}
+                                        </th>
+                                        <td className="px-6 py-4">
+                                            {tarifa.TipoVisita}
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            {tarifa.Estatus}
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            {tarifa.CategoriaPago}
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            {tarifa.Monto}
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            {tarifa.Moneda}
+                                        </td>
+                                        <td className="px-6 py-4 text-right">
+                                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                        </td>
+                                    </tr >
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            // Si aun no esta la data tira este mensaje
+            ) : ("Cargando información...")}
+
+            {/* this button is for TESTING the FETCH, you can copy and paste to try the fetch in your own file*/}
+            < button hidden id="get" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-4" onClick={handleTarifas} > Change Text</button >
+        </div >
     );
 };
 
 export default Tarifas;
-
-/* 
-        <div>
-            <h1 className="font-bold text-xl">Tarifas</h1>
-            <button id="get" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-4" onClick={getGet}>Change Text</button>
-            <p> {!data ? "Loading..." : data} </p>
-        </div>
-*/
