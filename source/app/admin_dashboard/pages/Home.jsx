13f07ff138e09e622212baee4df39b4ab6715594
Josué R.R <73318183+JosueRR@users.noreply.github.com>
2023-06-06 01:11:17 -0600

    Cambia mensaje de inicio del dashboard

--- source/app/admin_dashboard/pages/Home.jsx
+++ source/app/admin_dashboard/pages/Home.jsx
@@ -1,5 +1,7 @@
 const Home = () => {
-    return <h1>Este home page para el centro de control</h1>;
+    return (
+        <h1 class="font-lexend text-3xl font-thin" >¡Bienvenido al Centro de control!</h1>
+    )
 };
 
 export default Home;
