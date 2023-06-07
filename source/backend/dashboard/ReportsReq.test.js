b9e6ad92e364de65d9d5df8b5dca875cd47953fb
Marco Piedra Venegas <mpiedrav@proton.me>
2023-06-06 23:38:10 -0600

    Jest tests for profits and visits reports

--- source/backend/dashboard/ReportsReq.test.js
+++ source/backend/dashboard/ReportsReq.test.js
@@ -1,9 +1,26 @@
 const index = require("./ReportsReq")
 
-it("select profits in dates with no data", async () => {
-    await expect(index.selectProfitsInDateRange('1990-01-01', '1990-01-31')).resolves.toBe('[]')
+it("reports: profits with start date after end date", async () => {
+  await expect(index.selectVisitsInDateRange('2023-12-31', '2023-01-01')).resolves.toBe('[]')
 });
 
-it("select visits in dates with no data", async () => {
+it("reports: profits in dates with no data", async () => {
   await expect(index.selectVisitsInDateRange('1990-01-01', '1990-01-31')).resolves.toBe('[]')
+});
+
+it("reports: profits with invalid date formats", async () => {
+  await expect(index.selectVisitsInDateRange('00-00-0000', '99-99-9999')).resolves.toBe('[]')
+});
+
+
+it("reports: visits with start date after end date", async () => {
+  await expect(index.selectVisitsInDateRange('2023-12-31', '2023-01-01')).resolves.toBe('[]')
+});
+
+it("reports: visits in dates with no data", async () => {
+  await expect(index.selectVisitsInDateRange('1990-01-01', '1990-01-31')).resolves.toBe('[]')
+});
+
+it("reports: visits with invalid date formats", async () => {
+  await expect(index.selectVisitsInDateRange('00-00-0000', '99-99-9999')).resolves.toBe('[]')
 });
