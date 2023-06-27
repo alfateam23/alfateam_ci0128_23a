a5ea329b9ed1467e9fb0bae31a8fa2bba4d7c1a5
JoseAndreyP <joseandreypereira1999@gmail.com>
2023-06-27 00:26:30 -0600

    Added endpoints to class

--- source/app/admin_dashboard/pages/Horarios.jsx
+++ source/app/admin_dashboard/pages/Horarios.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState } from 'react';
 import { MobileTimePicker } from '@mui/x-date-pickers/MobileTimePicker';
 import dayjs from 'dayjs';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
@@ -7,19 +7,50 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 function Horarios() {
     const [horaEntrada, setHoraEntrada] = useState(null);
     const [horaSalida, setHoraSalida] = useState(null);
+    /*
+    useEffect(() => {
+      fetch('/backend/horarios/getHorarios')
+        .then((res) => {
+          if (!res.ok) {
+            console.log('Network response was not ok');
+          }
+          return res.json();
+        })
+        .then((data) => {
+          // Si los datos estan de esta forma { horaEntrada: '09:00', horaSalida: '17:00' }
+          const { horaEntrada, horaSalida } = data;
   
-    const handleHoraEntradaChange = (time) => {
+          setHoraEntrada(dayjs(horaEntrada, 'HH:mm'));
+          setHoraSalida(dayjs(horaSalida, 'HH:mm'));
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+        });
+    }, []);
+    */
+    const horaEntradaChange = (time) => {
       setHoraEntrada(time);
     };
-  
-    const handleHoraSalidaChange = (time) => {
+    
+    /*
+    useEffect(() => {
+      setHoraEntradaDB(horaEntrada);
+    }, [horaEntrada]);
+*/
+    const horaSalidaChange = (time) => {
       setHoraSalida(time);
     };
-  
+
+    /*
+    useEffect(() => {
+      setHoraSalidaDB(horaSalida);
+    }, [horaSalida]);
+
+    */
     const convertirADayjs = (time) => {
       return dayjs(time, 'HH:mm');
     };
-  
+
     return (
       <div>
         <h1 className="font-sans text-4xl rounded-none py-4 m-3" >Configuracion de horarios</h1>
@@ -31,7 +62,7 @@ function Horarios() {
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <MobileTimePicker
             value={horaEntrada ? convertirADayjs(horaEntrada) : null}
-            onChange={handleHoraEntradaChange}
+            onChange={horaEntradaChange}
           />
         </LocalizationProvider>
   
@@ -39,7 +70,7 @@ function Horarios() {
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <MobileTimePicker
             value={horaSalida ? convertirADayjs(horaSalida) : null}
-            onChange={handleHoraSalidaChange}
+            onChange={horaSalidaChange}
           />
         </LocalizationProvider>
   
