dea89d36a6601bc45403765376331568c7eb163d
HardJas88 <128011629+HardJas88@users.noreply.github.com>
2023-06-29 10:38:38 -0600

    Se hacen correcciones de code review

--- source/app/form/Review_page.js
+++ source/app/form/Review_page.js
@@ -1,10 +1,5 @@
 import { Descriptions } from "antd";
 import classNames from "classnames";
-import React, { useState, useEffect } from "react";
-import {
-  ContenedorBotonCentrado,
-  Boton,
-} from "./Elementos/ElementosFormulario";
 import "./style.css";
 import CalculateCost from "./CalculateVisitors";
 
@@ -28,9 +23,11 @@ export const Review_info = ({ UserData }) => {
       <div className="flex justify-center">
         <a
           href="#"
-          className="block max-w-lg p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+          className="block max-w-lg p-6 bg-white border border-gray-200 rounded-lg 
+              shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
         >
-          <dl className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 max-w-md text-gray-900 divide-y divide-gray-200 dark:text-white dark:divide-gray-700">
+          <dl className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 max-w-md
+               text-gray-900 divide-y divide-gray-200 dark:text-white dark:divide-gray-700">
             <div className={flexCol}>
               <dt className={classNames(textGray, fontSemiBold)}>
                 Nombre persona que reserva:
