0f83ed92ac59e1ee88c84bf2bc0a9a5a1b4f266f
JoseAndreyP <joseandreypereira1999@gmail.com>
2023-06-29 19:54:45 -0600

    Cambios propuestos para lista

--- source/app/admin_dashboard/pages/Lista/TablaReservaciones.jsx
+++ source/app/admin_dashboard/pages/Lista/TablaReservaciones.jsx
@@ -12,7 +12,7 @@ const TablaReservaciones = ({
     <table className="w-full text-sm text-center ">
 <thead className="text-xl text-gray-700 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
         <tr className="bg-[#FF8C32]">
-          <th scope="col" className="px-6 py-3 w-[250px]">
+          <th scope="col" className="px-6 py-3 w-[350px]">
             Código
             <button onClick={() => sortElements('ReservacionCodigo')}>
                 {sortField === 'ReservacionCodigo' && sortOrder === 'asc' ? '▲' : '▼'}
@@ -24,14 +24,17 @@ const TablaReservaciones = ({
               {sortField === 'TipoArea' && sortOrder === 'asc' ? '▲' : '▼'}
             </button>
           </th>
-          <th scope="col" className="px-6 py-3 w-[300px]">
+          <th scope="col" className="px-6 py-3 w-[380px]">
             Visitantes{' '}
             <button onClick={() => sortElements('TotalCantidadVisitantes')}>
                 {sortField === 'TotalCantidadVisitantes' && sortOrder === 'asc' ? '▲' : '▼'}
               </button>
           </th>
-          <th scope="col" className="px-6 py-3 w-[900px]">
-            Fecha entrada
+          <th scope="col" className="px-6 py-3 w-[500px]">
+            Fecha
+            <button onClick={() => sortElements('FechaInicio')}>
+                {sortField === 'FechaInicio' && sortOrder === 'asc' ? '▲' : '▼'}
+            </button>
           </th>
           <th scope="col" className="px-6 py-3 w-[250px]">
             Estado
