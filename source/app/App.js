7e51e79fa1b49b75ebdbc2e3c5387de5d4bf463a
JoAnPZ09 <70162434+JoAnPZ09@users.noreply.github.com>
2023-07-02 14:39:19 -0600

    Revert "Cupos"

--- source/app/App.js
+++ source/app/App.js
@@ -25,7 +25,6 @@ import Tarifas from './admin_dashboard/pages/Tarifas';
 import TarifasEditar from './admin_dashboard/pages/TarifasEditar';
 import Reports from './admin_dashboard/pages/Reports';
 import Users from './admin_dashboard/pages/users/usersList';
-import Cupos from './admin_dashboard/pages/Cupos';
 import UsersEdit from './admin_dashboard/pages/users/userListEdit';
 import UserCreate from './admin_dashboard/pages/users/UserCreate';
 
@@ -54,7 +53,6 @@ const AdminApp = () => {
                <Route path='/users/editar/:Cedula' element={<UsersEdit/>} />
                <Route path='/users/create' element={<UserCreate/>} />
                <Route path='/reports' element={<Reports />} />
-               <Route path='/Cupos' element={<Cupos />} />
                <Route path="/login" element={<Login />} />
             </Route>
          </Routes>
--- source/backend/index.js
+++ source/backend/index.js
@@ -14,8 +14,6 @@ const emailManager = require("./reservation/Email/emailRoutes");
 const bodyParser = require('body-parser');
 const reports = require('./dashboard/ReportsReq')
 const users = require('./users/users')
-const quota = require('./dashboard/QuotaReq');
-const schedule = require('./dashboard/ScheduleReq');
 const auth = require('./authentication/auth')
 
 dotenv.config();
@@ -46,8 +44,6 @@ app.use('/backend/users', users.router);
 //Signup and login
 app.use("/backend/email", emailManager);
 app.use("/backend/reports", reports.router);
-app.use("/backend/quota", quota.router);
-app.use("/backend/schedule", schedule.router);
 
 
 /* Authentication */
