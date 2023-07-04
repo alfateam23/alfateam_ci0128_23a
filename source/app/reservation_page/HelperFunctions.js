d99ba056ee68146f4b0a8e06e8783b6d416b2ef8
Ariel <ariel@debian-BULLSEYE-live-builder-AMD64>
2023-07-03 21:44:29 -0600

    fixes availability_page code

--- source/app/reservation_page/HelperFunctions.js
+++ source/app/reservation_page/HelperFunctions.js
@@ -34,3 +34,46 @@ export const ManageReservationOldUser = async (update,
     }, 4000);
   }
 };
+
+export const getAvailability = (userData, setDatesCapacity) => {
+  let apiParameters = [];
+  apiParameters.push(userData.start_date.toISOString())
+  userData.end_date !== '' ? apiParameters.push(
+    userData.end_date.toISOString()) : apiParameters.push('no');
+  apiParameters.push(userData.area)
+  
+  fetch(`/backend/capacity/${apiParameters[0]}/${apiParameters[1]}/${apiParameters[2]}`)
+  .then((res) => {
+    if (!res.ok) {
+      console.log('Network response was not ok');
+    }
+    return res.json();
+  })
+  .then((data) => setDatesCapacity(data))
+  .catch((error) => {
+    console.error('Error fetching data:', error);
+  });
+};
+
+export const updateCapacityValue = (prevCount,
+  count, datesCapacity, setDatesCapacity) => {
+  let operation = prevCount < count ? '+' : '-';
+  if (datesCapacity !== null) {
+    const newValues = datesCapacity.map(position => {
+      if (operation === '-') {
+        return {
+          ...position,
+          CupoOnlineDia: position.CupoOnlineDia + 1,
+        };
+      } else {
+        return {
+          ...position,
+          CupoOnlineDia: position.CupoOnlineDia - 1,
+        };
+      }
+    });
+    newValues.find((pos) => pos.CupoOnlineDia === 0) !== undefined ?
+    alert('No hay suficiente espacio') :
+    setDatesCapacity(newValues);
+  }
+}
